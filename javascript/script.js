ea9af4edc5c819c7a0547e8153c3c99bab00f6e1
sivasisdas <sivasisd7@gmail.com>
2023-08-12 13:12:15 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function openMenu(params) {
+   document.getElementById("navbar").style.left = "0px" 
+}
+
+function closeMenu(params) {
+    document.getElementById("navbar").style.left = "-280px"
+}
